30d605413ec405acdf9833f8bcc7311943b92e1a
Patrick Katthose <Gorgamol@users.noreply.github.com>
2020-02-28 19:05:06 +0100

    Init

---
+++ RucksackBackpack/RucksackBackpack.lua
@@ -0,0 +1,18 @@
+local f = CreateFrame("Frame")
+f:RegisterEvent("CHAT_MSG_PARTY")
+f:RegisterEvent("CHAT_MSG_SAY")
+f:RegisterEvent("CHAT_MSG_GUILD")
+f:RegisterEvent("CHAT_MSG_RAID")
+f:SetScript("OnEvent", function(self, event, msg, sender, _, _, _, _, _, chanNumber, ...)
+  if msg:match("Rucksack Backpack!") then
+    if event == "CHAT_MSG_PARTY" then
+      SendChatMessage("Backpack Rucksack!", "PARTY", nil, chanNumber)
+    elseif event == "CHAT_MSG_SAY" then
+      SendChatMessage("Backpack Rucksack!", "SAY", nil, chanNumber)
+    elseif event == "CHAT_MSG_GUILD" then
+      SendChatMessage("Backpack Rucksack!", "GUILD", nil, chanNumber)
+    elseif event == "CHAT_MSG_RAID" then
+      SendChatMessage("Backpack Rucksack!", "RAID", nil, chanNumber)
+    end
+  end
+end)
